ebb38ec53cc2af6c90cdb1f312c4c172e113cd89
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-01 17:24:28 +1000

    connect to firestore aand query the db (get all)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import { useEffect } from "react";
+import { collection, onSnapshot } from "@firebase/firestore";
+import db from "./firebase";
+
 function App() {
+	useEffect(() => {
+		onSnapshot(collection(db, "colors"), snapshot => {
+			console.log(snapshot.docs.map(doc => doc.data()));
+		});
+	});
+
 	return (
 		<div className="App">
 			<button className="button">New</button>
 			<ul>
 				<li>
-					<a href="#">edit</a>
+					<button>edit</button>
 					&nbsp;
 					<strong style={{ color: "#f00" }}>RED</strong>
 				</li>
 				<li>
-					<a href="#">edit</a>
+					<button>edit</button>
 					&nbsp;
 					<strong style={{ color: "#0f0" }}>GREEN</strong>
 				</li>
 				<li>
-					<a href="#">edit</a>
+					<button>edit</button>
 					&nbsp;
 					<strong style={{ color: "#00f" }}>BLUE</strong>
 				</li>
