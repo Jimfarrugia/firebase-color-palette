457458a0bd6dd2f7f45715478e1fae1627c5f5eb
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-01 16:54:13 +1000

    add basic layout to App view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 function App() {
 	return (
 		<div className="App">
-			<p>hello world</p>
+			<button className="button">New</button>
+			<ul>
+				<li>
+					<a href="#">edit</a>
+					&nbsp;
+					<strong style={{ color: "#f00" }}>RED</strong>
+				</li>
+				<li>
+					<a href="#">edit</a>
+					&nbsp;
+					<strong style={{ color: "#0f0" }}>GREEN</strong>
+				</li>
+				<li>
+					<a href="#">edit</a>
+					&nbsp;
+					<strong style={{ color: "#00f" }}>BLUE</strong>
+				</li>
+			</ul>
 		</div>
 	);
 }
